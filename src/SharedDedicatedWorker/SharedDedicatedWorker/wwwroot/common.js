23f9f0db44a906090c0dbd2590ddf3c8b933fbe5
Lance Collins <lancec@microsoft.com>
2022-02-17 05:14:41 -0800

    Start work on IPC.

---
+++ src/SharedDedicatedWorker/SharedDedicatedWorker/wwwroot/common.js
@@ -0,0 +1,130 @@
+﻿
+class PromiseCompletionSource {
+    Resolve;
+    Reject;
+    constructor() {
+        this.Promise = new Promise(function (resolve, reject) {
+            this.Resolve = resolve;
+            this.Reject = reject;
+        });
+    }
+}
+
+var NodeService = {
+    nextConnectionId: 0,
+    nextCallId: 0,
+    connections: {},
+    calls: {},
+    connect: function (port) {
+        var id = this.nextConnectionId++;
+        this.connections[id] = port;
+
+        port.onmessage = function (e) {
+            var d = e.data;
+            this[d.method](d.id, d.args);
+        };
+    },
+    disconnect: function (id) {
+        delete this.connections[id];
+    },
+    callClient: function (id, method, args) {
+        this.connections[id].postMessage({
+            method: method,
+            args: args
+        });
+    },
+    callClientAsync: function (id, method, args) {
+        var pcs = new PromiseCompletionSource();
+        var callId = this.nextCallId++;
+        calls[callId] = pcs;
+
+        this.connections[id].postMessage({
+            method: method,
+            args: args,
+            callId: callId
+        });
+
+        return pcs.Promise;
+    },
+    respond: function (args) {
+        var pcs = calls[args.callId];
+        if (args.resolveResult) {
+            pcs.Resolve(args.resolveResult);
+        } else {
+            pcs.Reject(args.RejectResult);
+        }
+    },
+    sendLog: function (id, args) {
+        console.log(`Message [${id}] -> [${args.target}]: ${args.message}`);
+        this.callClient(args.target, "writeLog", args.message);
+    },
+    onmessage: async function (e) {
+        var d = e.data;
+        if (d.callId) {
+            var response = await this[d.method](d.args);
+            
+        } else {
+            this[d.method](d.args);
+        }
+    },
+    writeLog: function (message) {
+        console.log(`writeLog: Message ${message}`);
+    }
+};
+
+
+function Promise() {
+    var deferred = {};
+    var promise = new Promise(function (resolve, reject) {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+    });
+    deferred.promise = promise;
+    return deferred;
+}
+
+class PageNodeService {
+    Target;
+    constructor(target) {
+        this.Target = target;
+    }
+}
+
+class WorkerNodeClient {
+    Worker;
+    constructor(startupfile) {
+        this.Worker = new Worker(startupFile);
+    }
+}
+
+class WorkerNodeService {
+
+}
+
+class SharedWorkerNodeClient {
+    Worker;
+    constructor(startupfile) {
+        this.Worker = new Worker(startupFile);
+    }
+}
+
+class SharedWorkerNodeService {
+
+}
+
+onconnect = function (e) {
+    var port = e.ports[0];
+    NodeService.connect()
+
+    console.log(`Connected.`);
+
+    port.onmessage = function (e) {
+        var d = e.data;
+        NodeService[d.method](d.id, port, d.args);
+
+        console.log(`Received [${d.id}] ${d.method}: '${d.args}'`);
+        if (d.method !== "disconnect") {
+            callClient(d.id, "message", `ACK [${d.id}] ${d.method}: '${d.args}'`);
+        }
+    };
+}
---
+++ src/SharedDedicatedWorker/SharedDedicatedWorker/wwwroot/ipc.ts
@@ -0,0 +1,58 @@
+﻿namespace Codex.Ipc {
+    class PromiseCompletionSource {
+        Resolve: any;
+        Reject: any;
+        Task: Promise<any>;
+        constructor() {
+            var self = this;
+            this.Task = new Promise(function (resolve, reject) {
+                self.Resolve = resolve;
+                self.Reject = reject;
+            });
+        }
+    }
+
+    interface IMessage {
+        id: number,
+        method: string,
+        args: any,
+        callId: any
+    }
+
+    class IpcServer<T> {
+        public Target: T;
+        private nextConnectionId = 0;
+        private nextCallId = 0;
+        private connections = new Map<number, MessagePort>();
+        private pendingCalls = new Map<number, PromiseCompletionSource>();
+
+        public connect(port: MessagePort) {
+            var id = this.nextConnectionId++;
+            this.connections.set(id, port);;
+            var server = this;
+
+            port.onmessage = function (ev) {
+                var d: IMessage = ev.data;
+                server.Target[d.method](d.id, d.args);
+            };
+        }
+
+        public disconnect(id: number) {
+            this.connections.delete(id);
+        }
+
+        public callClient(m: IMessage) {
+            this.connections.get(m.id).postMessage(m);
+        }
+
+        public callClientAsync<T>(m: IMessage) : Promise<T> {
+            m.callId = this.nextCallId++;
+            var pcs = new PromiseCompletionSource();
+            this.pendingCalls.set(m.callId, pcs);
+
+            this.connections.get(m.id).postMessage(m);
+            return pcs.Task;
+        }
+
+    }
+}
